634ddd695df96361971d9551e6f2039c1724f6a0
Jonas Gunz <himself@jonasgunz.de>
2021-01-13 21:10:07 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
